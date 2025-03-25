b3a8242fb0335a54bdcfdd32e4c80c262dfad103
ApurboShib <shibjoy088@gmail.com>
2025-03-25 16:10:56 +0600

    first commit

--- explore-react/src/App.jsx
+++ explore-react/src/App.jsx
@@ -8,33 +8,27 @@ function App() {
 
   return (
     <>
-    
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
       <h1>Vite + React</h1>
-      <Person></Person>
-      <Student></Student>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  )
-}
-
-function Person() {
-  const age = 100;
-  return (
-    <div>
-      <h1>My name is Joy shib with age {age}.</h1>
-     
-    </div>
-  )
-}
-
-function Student() {
-  return (
-    <div>
-      <h1>My favourite student details.</h1>
-      <p> Name : Tahnid Ahmed</p>
-      <p>Age : 23</p>
-      <p>School : Scholarshome.</p>
-     
-    </div>
   )
 }
 
