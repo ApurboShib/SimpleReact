6624688dd5f88cd5312beb4f68f4d79c6169f085
ApurboShib <shibjoy088@gmail.com>
2025-03-25 16:26:46 +0600

    added

--- explore-react/src/App.jsx
+++ explore-react/src/App.jsx
@@ -6,41 +6,40 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  function Student() {
+    const name = "John Doe";
+    const age = 25;
+    const country = "USA";
+  
+    return (
+      <div>
+        <h2>Student</h2>
+        <p>Student Name: {name}</p>
+        <p>Age: {age}</p>
+        <p>Country: {country}</p>
+      </div>
+    );
+  }
+
+  function Counter() {
+    let cnt = 0;
+    return (
+      <div>
+        <h2>Counter</h2>
+        <p>Count: {cnt}</p>
+        <button>Increment</button>
+        <button>Decrement</button>
+      </div>
+    );
+  }
+
   return (
     <>
-      
       <h1>Vite + React</h1>
       <Student></Student>
       <Counter></Counter>
-      
     </>
   )
 }
 
-function Student() {
-  const name = "John Doe";
-  const age = 25;
-  const country = "USA";
-  
-  return (
-    <div>
-      <h2>Student </h2>
-      <p>Student Name : {name}</p>
-      <p>Age : { age}</p>
-      <p>Country : {country} </p>
-    </div>
-  )
-}
-
-function Counter() {
-  let cnt = 0;
-  return (
-    <div>
-      <h2>Counter</h2>
-      <p>Count : {cnt}</p>
-      <button>Increment</button>
-      <button>Decrement</button>
-    </div>
-  )
-}
 export default App
